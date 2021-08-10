f0fbae2cf691964271e58c6b840af9645cbccc9f
La Xuan Hau <laxuanhau1995@gmail.com>
2021-08-10 15:17:46 +0700

    a

--- components/Feed/RoomChat.js
+++ components/Feed/RoomChat.js
@@ -46,7 +46,7 @@ import { UsersIcon } from "@heroicons/react/solid";
 function RoomChat() {
   return (
     <div className="shadow bg-white dark:bg-[#242526] p-4 rounded-xl relative">
-      <div className="flex items-stretch justify-start overflow-x-scroll scrollbar-hide space-x-2  w-[300px]">
+      <div className="flex items-stretch justify-start overflow-x-scroll scrollbar-hide space-x-2  w-[300px] md:w-[560px]">
         <button className="border dark:hover:bg-[#3a3b3c] dark:border-blue-500 dark:border-opacity-40 border-blue-100 rounded-full px-3 py-2 flex items-center hover:bg-blue-50 duration-300 whitespace-nowrap">
           <UsersIcon className="h-5 mr-2 text-pink-500" />{" "}
           <p className="text-xs font-semibold text-blue-500">Create Room</p>
